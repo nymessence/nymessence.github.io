67e981eb8a4e3cef3a5e33b347d1298370f245fd
nymessence <humid_ray_0u@icloud.com>
2025-08-14 19:11:12 -0400

    setting up vite deployment

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,13 @@ import { defineConfig } from 'vite';
 import rewriteAll from 'vite-plugin-rewrite-all';
 
 export default defineConfig({
-  // Set the base path to ensure relative URLs work correctly for GitHub Pages.
-  base: './',
-  
-  // This tells Vite to look for your HTML files in the project's root directory.
-  root: './',
-
-  // The build configuration will output to the 'dist' folder.
-  build: {
-    outDir: 'dist',
-    
-    // This setting ensures that the output paths are relative.
-    assetsDir: '',
-  },
-
-  // Use the plugin to automatically rewrite all paths.
   plugins: [
     rewriteAll()
   ],
+  // Add other configurations here if needed
+  build: {
+    // You might want to add a base path for GitHub Pages
+    // base: '/nymessence.github.io/',
+  }
 });
+
