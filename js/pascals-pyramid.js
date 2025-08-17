d25f2e83efac86ed5469abc1f8656c1440a6cbd7
nymessence <humid_ray_0u@icloud.com>
2025-08-16 20:52:37 -0400

    update

--- js/pascals-pyramid.js
+++ js/pascals-pyramid.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls.js";
+import * as THREE from "../node_modules/three/build/three.module.js";
+import { PointerLockControls } from "../node_modules/three/examples/jsm/controls/PointerLockControls.js";
 
 const container = document.getElementById('container');
 const scene = new THREE.Scene();
--- main.js
+++ main.js
@@ -5,10 +5,10 @@
 
 // Import all necessary Three.js dependencies from node_modules.
 // The relative paths have been corrected to be relative to the root of your project.
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { ConvexGeometry } from "three/examples/jsm/geometries/ConvexGeometry.js";
-import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls.js";
+import * as THREE from "../node_modules/three/build/three.module.js";
+import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+import { ConvexGeometry } from "../node_modules/three/examples/jsm/geometries/ConvexGeometry.js";
+import { PointerLockControls } from "../node_modules/three/examples/jsm/controls/PointerLockControls.js";
 
 // Import all your custom JavaScript files from the 'js' directory.
 // The paths are now correctly relative to the root of your project.
